cc741a13fec4c8e2e3d34edf1fa42514f20e262d
Jon D. Crowder <rccommandotac@gmail.com>
2023-04-15 10:51:17 -0500

    fix package.json main, export everything from mod.d.ts

---
+++ mod.d.ts
@@ -0,0 +1,10 @@
+
+export  * from "./color";
+export  * from "./entity";
+export  * from "./item";
+export  * from "./mt";
+export  * from "./node";
+export  * from "./object";
+export  * from "./player";
+export  * from "./sound";
+export  * from "./vector";
