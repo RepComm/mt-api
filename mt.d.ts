1be014f415d3bb9dd39695a025a0c72097923653
Jon D. Crowder <rccommandotac@gmail.com>
2023-04-23 12:58:40 -0500

    minetest.register_chatcommand

--- mt.d.ts
+++ mt.d.ts
@@ -13,13 +13,42 @@ export type MtInsecEnv = typeof globalThis;
 
 export type Raycast = LuaIterable<MtPointedThing>;
 
+export interface Privs {
+  [key: string]: boolean;
+}
+
+export interface ChatCommandDef {
+  /**
+   * Short parameter description
+   * "<name> <privilege>"
+   */
+  params: string;
+  /**
+   * Full description
+   * "Remove privilege from player"
+   */
+  description: string;
+
+  /**
+   * Require the "privs" privilege to run
+   * {privs=true}
+   */
+  privs?: Privs;
+
+  /**
+   * Called when command is run
+   * Returns boolean success and text output.
+   */
+  func: (name: string, param: string) => LuaMultiReturn<[boolean, string]>;
+}
+
 export interface MinetestGlobal {
   register_on_joinplayer(this: void, cb: MtPlayerJoinCallback): void;
   register_on_leaveplayer(this: void, cb: MtPlayerLeaveCallback): void;
-  register_on_shutdown (this: void, cb: ()=> void): void;
+  register_on_shutdown(this: void, cb: () => void): void;
   chat_send_player(this: void, playerName: string, message: string): void;
   chat_send_all(this: void, message: string): void;
-  
+
   request_insecure_environment(this: void): MtInsecEnv;
   get_modpath(this: void, modname: string): string;
   get_current_modname(this: void): string;
@@ -33,21 +62,24 @@ export interface MinetestGlobal {
   add_entity(this: void, pos: MtVec3, name: string, staticdata?: MtEntityStaticData): MtObjRef;
   register_entity(this: void, name: string, enDef: Partial<MtEntityDef>): void;
   register_globalstep(this: void, cb: (this: void, dtime: number) => void): void;
-  get_player_by_name (this: void, name: string): MtPlayer|undefined;
-  get_node_or_nil (this: void, pos: MtVec3): MtNode|undefined;
-  get_node (this: void, pos: MtVec3): MtNode;
+  get_player_by_name(this: void, name: string): MtPlayer | undefined;
+  get_node_or_nil(this: void, pos: MtVec3): MtNode | undefined;
+  get_node(this: void, pos: MtVec3): MtNode;
 
-  after(this: void, time: number, func: ()=> void): void;
-  get_gametime (this: void): number;
+  after(this: void, time: number, func: () => void): void;
+  get_gametime(this: void): number;
 
   raycast(this: void, from: MtVec3, to: MtVec3, objects?: boolean, liquids?: boolean): Raycast;
 
-  luaentities: {[key: string]: MtObjRef};
-  object_refs: {[key: string]: MtObjRef};
+  luaentities: { [key: string]: MtObjRef };
+  object_refs: { [key: string]: MtObjRef };
 
-  add_particle (this: void, partDef: MtParticleDef): number;
+  add_particle(this: void, partDef: MtParticleDef): number;
   add_particlespawner(this: void, partSpawnDef: MtParticleSpawnerDef): number;
-  delete_particlespawner(id: number, playername?: string): void;
+  delete_particlespawner(this: void, id: number, playername?: string): void;
+  unregister_chatcommand(this: void, name: string): void;
+  register_privilege(this: void, name: string, cfg: string|{ description: string; give_to_singleplayer: boolean}): void;
+  register_chatcommand(this: void, name: string, cmd: ChatCommandDef): void;
 }
 
 declare global {
